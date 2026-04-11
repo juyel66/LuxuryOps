2c2b45ef87da26db76581e71d0ab061829668647
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-12 02:40:51 +0600

    fix: reorganize imports and component rendering order in HomePage for improved structure

--- app/(WithCommonLayout)/page.tsx
+++ app/(WithCommonLayout)/page.tsx
@@ -1,4 +1,9 @@
+import ExperienceTheHeight from "../Pages/WebsiteComponents/HomePage/ExperienceTheHeight";
 import HomePageHeroSections from "../Pages/WebsiteComponents/HomePage/HomePageHeroSections";
+import OurFleetHome from "../Pages/WebsiteComponents/HomePage/OurFLeetHome";
+import TestimonialHome from "../Pages/WebsiteComponents/HomePage/TestimonialHome";
+import TheVVIPLuxuryExperience from "../Pages/WebsiteComponents/HomePage/TheVVIPLuxuryExperience";
+import TheVVIPSignatureJourney from "../Pages/WebsiteComponents/HomePage/TheVVIPSignatureJourney";
 import VVIPLuxuryService from "../Pages/WebsiteComponents/HomePage/VVIPLuxuryService";
 
 
@@ -7,6 +12,11 @@ const HomePage = () => {
         <div>
             <HomePageHeroSections />
             <VVIPLuxuryService />
+            <TheVVIPLuxuryExperience />
+            <TheVVIPSignatureJourney />
+            <OurFleetHome />
+            <TestimonialHome />
+            <ExperienceTheHeight />
             
         </div>
     );
