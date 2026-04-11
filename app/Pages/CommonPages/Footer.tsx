233f41c3645becf058304c072fc128b0a2ed5b51
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-12 02:41:09 +0600

    fix: refine footer layout and styling for improved readability and structure

--- app/Pages/CommonPages/Footer.tsx
+++ app/Pages/CommonPages/Footer.tsx
@@ -9,41 +9,41 @@ import arrowUp from "../../../public/images/arrow-up-line.svg";
 const Footer = () => {
   return (
    <div>
-    <footer className="w-full bg-black text-white px-4 sm:px-6 md:px-6 py-2 ">
-      <div className="container mx-auto relative">
+    <footer className="w-full px-4 py-2 text-white bg-black sm:px-6 md:px-6 ">
+      <div className="container relative mx-auto">
 
         {/* Back to top */}
         <div className="flex justify-center mb-8 md:mb-10 mt-14 md:mt-20">
           <button
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-            className="text-sm mb-20 flex justify-center items-center cursor-pointer md:mb-40 text-gray-400 hover:text-white transition"
+            className="flex items-center justify-center mb-20 text-xl text-gray-400 transition cursor-pointer font-cardo md:mb-40 hover:text-white"
           >
             <Image src={arrowUp} alt="Back to top" className="w-5 h-5" /> Back to top
           </button>
         </div>
 
         {/* Center SVG */}
-        <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
+        <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
           <div className="w-[220px] mt-16 sm:w-[320px] md:w-[520px] lg:w-[620px] opacity-25">
             <Image
               src={footer}
               alt="VIP Logo"
-              className="w-full h-full object-contain"
+              className="object-contain w-full h-full"
               priority
             />
           </div>
         </div>
 
         {/* PERFECT 3-COLUMN ROW */}
-        <div className="relative  z-10 mb-10 md:mb-10 w-full flex flex-col md:flex-row md:items-center md:justify-between gap-5 md:gap-6 text-sm text-gray-400">
+        <div className="relative z-10 flex flex-col w-full gap-5 mb-10 text-sm text-gray-400 md:mb-10 md:flex-row md:items-center md:justify-between md:gap-6">
           
           {/* LEFT */}
-          <div className="w-full mb-10 md:flex-1 text-center md:text-left">
+          <div className="w-full mb-10 text-center md:flex-1 md:text-left">
             <p>© 2026 VVIP Luxury. All rights reserved.</p>
           </div>
 
           {/* CENTER */}
-          <div className="w-full md:flex-1 mb-10  flex flex-wrap justify-center gap-6 md:gap-8">
+          <div className="flex flex-wrap justify-center w-full gap-6 mb-10 md:flex-1 md:gap-8">
             <Link href="/">Home</Link>
             <Link href="/about">About VVIP</Link>
             <Link href="/fleet">Our Fleet</Link>
@@ -52,11 +52,11 @@ const Footer = () => {
           </div>
 
           {/* RIGHT */}
-          <div className="w-full md:flex-1 flex justify-center md:justify-end items-center gap-2">
+          <div className="flex items-center justify-center w-full gap-2 md:flex-1 md:justify-end">
             <span className="text-base">
               <Image src={instagram} alt="Instagram" className="w-5 h-5" />
             </span>
-            <Link href="/privacy" className="hover:text-white transition">
+            <Link href="/privacy" className="transition hover:text-white">
               Privacy Policy
             </Link>
           </div>
