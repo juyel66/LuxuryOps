aec119cbd39703f08b4ac1547f077c2f298f5399
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-12 07:07:45 +0600

    Add services page component to display service offerings

---
+++ app/(WithCommonLayout)/services/page.tsx
@@ -0,0 +1,15 @@
+import ServiceCard from "@/app/Pages/WebsiteComponents/Services/ServiceCard";
+import ServicesHomePage from "@/app/Pages/WebsiteComponents/Services/ServicesHomePage";
+
+
+const page = () => {
+    return (
+        <div>
+            <ServicesHomePage />
+            <ServiceCard />
+            
+        </div>
+    );
+};
+
+export default page;
