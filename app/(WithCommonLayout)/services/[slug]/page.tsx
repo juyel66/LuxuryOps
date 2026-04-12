85606954ac8c57ebf302c9a9d0cda688312a0f9d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-13 04:59:28 +0600

    Refactor service details page to include multiple components for enhanced user experience

--- app/(WithCommonLayout)/services/[slug]/page.tsx
+++ app/(WithCommonLayout)/services/[slug]/page.tsx
@@ -1,9 +1,21 @@
+import ExperienceTheHeight from '@/app/Pages/WebsiteComponents/HomePage/ExperienceTheHeight';
+import TestimonialHome from '@/app/Pages/WebsiteComponents/HomePage/TestimonialHome';
+import ServiceDetailsHeroSection from '@/app/Pages/WebsiteComponents/Services/ServiceDetailsHeroSection';
+import ServiceGallery from '@/app/Pages/WebsiteComponents/Services/ServiceGallery';
+import ServiceOverview from '@/app/Pages/WebsiteComponents/Services/ServiceOverView';
+import YouMayAlsoLike from '@/app/Pages/WebsiteComponents/Services/YouMayAlsoLike';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            This is details page 
+            <ServiceDetailsHeroSection />
+            <ServiceOverview />
+            <ServiceGallery />
+            <TestimonialHome />
+            <YouMayAlsoLike />
+            <ExperienceTheHeight />
+
             
         </div>
     );
