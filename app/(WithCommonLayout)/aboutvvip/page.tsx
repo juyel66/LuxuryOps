44539a5f40209d8af2917446320fcac3f65b6204
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-12 07:07:16 +0600

    Add ExperienceTheHeight and TestimonialHome components to the about VVIP page

--- app/(WithCommonLayout)/aboutvvip/page.tsx
+++ app/(WithCommonLayout)/aboutvvip/page.tsx
@@ -2,6 +2,8 @@ import AboutVVIPHome from '@/app/Pages/WebsiteComponents/AboutVVIP/AboutVVIPHome
 import MeatTheTeam from '@/app/Pages/WebsiteComponents/AboutVVIP/MeetTheTeam';
 import OurValues from '@/app/Pages/WebsiteComponents/AboutVVIP/OurValues';
 import WhyVVIP from '@/app/Pages/WebsiteComponents/AboutVVIP/WhyVVIP';
+import ExperienceTheHeight from '@/app/Pages/WebsiteComponents/HomePage/ExperienceTheHeight';
+import TestimonialHome from '@/app/Pages/WebsiteComponents/HomePage/TestimonialHome';
 import TheVVIPSignatureJourney from '@/app/Pages/WebsiteComponents/HomePage/TheVVIPSignatureJourney';
 import React from 'react';
 
@@ -13,6 +15,8 @@ const page = () => {
             <OurValues />
             <TheVVIPSignatureJourney />
             <MeatTheTeam /> 
+            <TestimonialHome />
+            <ExperienceTheHeight />
     
             
         </div>
