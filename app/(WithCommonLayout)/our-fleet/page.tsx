2968907ab3ffd919c16edb78d4c5c2fe9d0bcf0d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-12 07:07:24 +0600

    Add OurFleet component to display fleet details and enhance user experience

---
+++ app/(WithCommonLayout)/our-fleet/page.tsx
@@ -0,0 +1,18 @@
+import ExperienceTheHeight from "@/app/Pages/WebsiteComponents/HomePage/ExperienceTheHeight";
+import OurFleetCard from "@/app/Pages/WebsiteComponents/OurFleet/OurFleetCard";
+import OurFleetPage from "@/app/Pages/WebsiteComponents/OurFleet/OurFleetPage";
+
+
+const OurFleet = () => {
+    return (
+        <div>
+            <OurFleetPage />
+            <OurFleetCard />
+            <ExperienceTheHeight />
+            
+            
+        </div>
+    );
+};
+
+export default OurFleet;
