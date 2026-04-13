b93b8c90563f53df7629a392bf46b28009e02fe0
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-14 03:57:45 +0600

    Add Terms and Conditions page component

---
+++ app/(WithCommonLayout)/terms-and-conditions/page.tsx
@@ -0,0 +1,15 @@
+import TermsAndConditions from "@/app/Pages/WebsiteComponents/TermsAndConditions/TermsAndConditions";
+import TermsAndConditionsHomePage from "@/app/Pages/WebsiteComponents/TermsAndConditions/TermsAndConditionsHomePage";
+
+
+const page = () => {
+    return (
+        <div>
+            <TermsAndConditionsHomePage />
+            <TermsAndConditions />
+            
+        </div>
+    );
+};
+
+export default page;
