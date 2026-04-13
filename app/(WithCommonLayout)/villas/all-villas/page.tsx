ec2259da1c3c539fb4bede9a420ba993fa389471
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-14 01:56:37 +0600

    Add page component for displaying AllVillas and VillaEnquiry

---
+++ app/(WithCommonLayout)/villas/all-villas/page.tsx
@@ -0,0 +1,15 @@
+import AllVillas from "@/app/Pages/WebsiteComponents/Villas/AllVilla";
+import VillaEnquiry from "@/app/Pages/WebsiteComponents/Villas/VillaEnquiry";
+
+
+const page = () => {
+    return (
+        <div>
+            <AllVillas />
+            <VillaEnquiry />
+            
+        </div>
+    );
+};
+
+export default page;
