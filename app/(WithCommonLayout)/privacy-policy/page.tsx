f2f095032e5103b1766639f833a3302a21c8af89
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-14 03:57:38 +0600

    Add Privacy Policy page component for website

---
+++ app/(WithCommonLayout)/privacy-policy/page.tsx
@@ -0,0 +1,15 @@
+import PrivacyPolicy from "@/app/Pages/WebsiteComponents/PrivacyPolicy.tsx/PrivacyPolicy";
+import PrivacyPolicyHomePage from "@/app/Pages/WebsiteComponents/PrivacyPolicy.tsx/PrivacyPolicyHomePage";
+
+
+const page = () => {
+    return (
+        <div>
+            <PrivacyPolicyHomePage />
+            <PrivacyPolicy />
+            
+        </div>
+    );
+};
+
+export default page;
