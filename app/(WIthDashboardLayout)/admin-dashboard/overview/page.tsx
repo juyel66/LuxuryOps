b8fbc57ed8119d4de70e4baafdbc704b93425eab
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-14 07:03:41 +0600

    Add overview page component with basic layout

---
+++ app/(WIthDashboardLayout)/admin-dashboard/overview/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="container mx-auto text-white ">
+      This is over view page 
+      
+    </div>
+  );
+};
+
+export default page;
