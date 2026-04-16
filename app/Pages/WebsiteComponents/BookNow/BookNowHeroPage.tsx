bb152d87d00f9dff82626d89efafeb643d179e84
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-16 07:03:28 +0600

    Adjust padding in BookNowHeroPage component for improved layout

--- app/Pages/WebsiteComponents/BookNow/BookNowHeroPage.tsx
+++ app/Pages/WebsiteComponents/BookNow/BookNowHeroPage.tsx
@@ -19,7 +19,7 @@ export default function BookNowHeroPage() {
       <div className="absolute inset-0 bg-black/20" />
 
       {/* CONTENT */}
-      <div className="relative z-10 flex items-center h-full px-6 sm:px-10 lg:px-20">
+      <div className="relative z-10 flex items-center h-full px-4 ">
         <div className="container flex items-center justify-between w-full mx-auto">
    
           <div className="container mx-auto">
