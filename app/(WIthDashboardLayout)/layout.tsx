4ab185d8d120af04add16b02b8c446d097a74d1d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-16 06:59:49 +0600

    Refactor layout component for improved structure and readability

--- app/(WIthDashboardLayout)/layout.tsx
+++ app/(WIthDashboardLayout)/layout.tsx
@@ -5,11 +5,11 @@ import { AppSidebar } from "@/components/app-sidebar";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full">
+      <div className="flex w-full min-h-screen">
         <AppSidebar />
 
         {/* MAIN CONTENT FIXED */}
-        <main className="flex-1 w-full flex justify-center items-start p-6 border border-gray-700 bg-black ">
+        <main className="flex items-start justify-center flex-1 w-full p-6 bg-black border border-gray-700 ">
           
           {/* Trigger stays on top-left */}
           <div className="absolute left-4 top-4">
@@ -18,6 +18,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
          
           <div className="w-full ">
+            <div className="p-3 text-2xl text-center border-2">This is dashboard navbar </div>
             {children}
           </div>
 
