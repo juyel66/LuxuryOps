3f9296f40410ac2cedd79a7e11cc239721de2968
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-16 07:00:59 +0600

    Add Fleet Management page component for displaying fleet details and features

---
+++ app/(WIthDashboardLayout)/admin-dashboard/fleet-management/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import AdminFleetDetailsFeature from "@/app/Pages/DashboardComponent/AdminComponents/AdminFleetDetailsFeatures";
+import AdminFleetDetailsHero from "@/app/Pages/DashboardComponent/AdminComponents/AdminFleetDetailsHero";
+
+
+const page = () => {
+    return (
+        <div>
+            <AdminFleetDetailsHero />
+            <AdminFleetDetailsFeature />
+            
+        </div>
+    );
+};
+
+export default page;
