b0c827710fdfcdaf27212bf0211f286b40793f2c
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-16 07:04:36 +0600

    Adjust padding in TermsAndConditions component for improved layout

--- app/Pages/WebsiteComponents/TermsAndConditions/TermsAndConditions.tsx
+++ app/Pages/WebsiteComponents/TermsAndConditions/TermsAndConditions.tsx
@@ -2,14 +2,14 @@
 
 export default function TermsAndConditions() {
   return (
-    <section className="w-full bg-[#05070B] text-white py-16 px-4 md:px-16 lg:px-24">
+    <section className="w-full bg-[#05070B] text-white py-16 px-4 ">
       
       <div className="container grid grid-cols-1 mx-auto border-t md:grid-cols-2 border-white/10">
 
         {terms.map((item, i) => (
           <div
             key={i}
-            className="p-8 border-b border-white/10 "
+            className="p-4 border-b border-white/10 "
           >
             {/* GOLD LINE */}
             <div className="w-10 h-[2px] bg-[#D4AF37] mb-4" />
