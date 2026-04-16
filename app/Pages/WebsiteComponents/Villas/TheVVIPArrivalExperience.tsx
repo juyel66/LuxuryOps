9218e5b56a137f4f609826b5692855f936a983a6
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-16 07:04:50 +0600

    Adjust padding in TheVVIPArrivalExperience component for improved layout

--- app/Pages/WebsiteComponents/Villas/TheVVIPArrivalExperience.tsx
+++ app/Pages/WebsiteComponents/Villas/TheVVIPArrivalExperience.tsx
@@ -8,7 +8,7 @@ export default function TheVVIPArrivalExperience() {
 
 
   return (
-    <section className="w-full  bg-[#05070B] text-white py-20 px-6 md:px-16 lg:px-24 relative overflow-hidden">
+    <section className="w-full  bg-[#05070B] text-white py-20 px-4 relative overflow-hidden">
       
       <div className="grid items-center grid-cols-1 gap-12 mx-auto max-w-7xl lg:grid-cols-2">
         
