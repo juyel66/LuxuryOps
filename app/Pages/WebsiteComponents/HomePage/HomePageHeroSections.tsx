c2e51cfd3fe1e07ca7adaeb36270d9ecf74c6d73
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-20 05:35:22 +0600

    Add spacing for improved readability in HomePageHeroSections component

--- app/Pages/WebsiteComponents/HomePage/HomePageHeroSections.tsx
+++ app/Pages/WebsiteComponents/HomePage/HomePageHeroSections.tsx
@@ -5,6 +5,9 @@ import rightArrow from "../../../../public/images/Component 3 copy.svg";
 import Image from "next/image";
 
 
+
+
+
 const HomePageHeroSections = () => {
   return (
     <section className=" relative w-full h-[82vh] overflow-hidden  text-white">
