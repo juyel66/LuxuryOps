8bbd71efff4bde22500e59ff57b7ba30f368a63a
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-21 04:59:27 +0600

    Remove commented-out padding adjustment in DashboardLayout

--- app/(WithCommonLayout)/layout.tsx
+++ app/(WithCommonLayout)/layout.tsx
@@ -6,7 +6,8 @@ const DashboardLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <div>
             <Navbar />
-            <div className="min-h-screen bg-[#121212]  md:pt-24 ">
+            {/* <div className="min-h-screen bg-[#121212]  md:pt-24 "> */}
+            <div className="min-h-screen bg-[#121212]  ">
                 {children}
             </div>
             <Footer />
