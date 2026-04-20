5ffedcb90c2071920c3d83d43b94b002f6f6b13d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-21 05:48:12 +0600

    Update NotFound component to enhance 404 heading size for better visibility

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center bg-[#05070B] text-white px-4">
-      <h2 className="mb-4 text-5xl font-semibold">404</h2>
+      <h2 className="mb-4 font-semibold text-7xl">404</h2>
       <p className="max-w-md mb-6 text-center text-gray-400">
         Page not found
       </p>
