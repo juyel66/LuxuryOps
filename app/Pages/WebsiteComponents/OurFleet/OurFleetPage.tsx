7410ef036caa85a983a5779726adf80442a26c54
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-24 05:56:36 +0600

    Add animation to scroll indicator in OurFleet component for enhanced user experience

--- app/Pages/WebsiteComponents/OurFleet/OurFleetPage.tsx
+++ app/Pages/WebsiteComponents/OurFleet/OurFleetPage.tsx
@@ -39,7 +39,7 @@ export default function OurFleet() {
           </div>
 
           {/* RIGHT SCROLL INDICATOR */}
-          <div className="flex-col items-center hidden gap-4 lg:flex">
+          <div className="flex-col items-center hidden gap-4 lg:flex animate-[bounce_2s_ease-in-out_infinite]">
             <span className="text-sm tracking-widest rotate-90 text-white/70">
               Scroll
             </span>
